4c004c4af85d370255916378db4d9a9c0a20daef
Amna-Ayari <ayariamna2018@gmail.com>
2021-05-19 01:30:17 +0200

    addfil

---
+++ src/components/ListsMovies.js
@@ -0,0 +1,25 @@
+export const ListsMovies =[{
+    id:Math.random(),
+    title:"The Lord of the Rings",
+    description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    posterUrl:"https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_.jpg",
+    rate:5
+
+},
+{
+    id:Math.random(),
+    title:"Resident Evil",
+    description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    posterUrl:"https://i1.wp.com/3.bp.blogspot.com/_X8ggyq9cwIk/TIn7Vpj9B2I/AAAAAAAAAAM/rcvELHwY26g/s1600/Resident+Evil+5+Movie.jpg?ssl=1",
+    rate:4
+
+},
+{
+    id:Math.random(),
+    title:"SAW",
+    description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    posterUrl:"https://i.ytimg.com/vi/ybRO7jwN4aw/maxresdefault.jpg",
+    rate:5
+
+}
+]
